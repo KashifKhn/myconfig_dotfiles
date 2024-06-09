fa64999c4ffad0d4b78aea55a4792e9aaaa8918a
KashifKhn <kashifkhnx04@gmail.com>
2024-06-09 17:07:23 +0500

    init commit

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" }) -- Add any additional keymaps here
+
+keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
